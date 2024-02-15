e1ae93d46be5fe305ecbb197b9d6e462c0b44dc0
nv22051-ali-safar <147384419+nv22051-ali-safar@users.noreply.github.com>
2024-02-15 06:55:08 +0000

    student card

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import Card from './components/Card';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <Card/>
+      </header>
+    </div>
+  );
+}
+
+export default App;
