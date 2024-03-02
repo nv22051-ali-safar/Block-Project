e6e98ae5d733897e71dbbdd1e5e0119f61f53427
nv22051-ali-safar <147384419+nv22051-ali-safar@users.noreply.github.com>
2024-03-02 16:59:04 +0000

    Article card component

--- src/constants/images.js
+++ src/constants/images.js
@@ -1,8 +1,13 @@
 import Logo from '../assests/Logo.svg'
 import HeroImage from '../assests/HeroImage.svg'
+import Post1Image from '../assests/posts/post1.jpg'
+import PostProfileImage from '../assests/posts/post-profile.svg'
+
 const images = {
     Logo,
     HeroImage,
+    Post1Image,
+    PostProfileImage,
 };
 
 export default images;
