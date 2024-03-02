fc11faf4aeed005e5eda716dd51ff5ca7f8d353e
nv22051-ali-safar <147384419+nv22051-ali-safar@users.noreply.github.com>
2024-03-02 18:09:53 +0000

    CTA Section

--- src/components/ArticleCard.jsx
+++ src/components/ArticleCard.jsx
@@ -7,7 +7,7 @@ const ArticleCard = ({ className }) => {
         <div className={`rounded-xl overflow-hidden shadow-[rgba(7,_65,_210,_0.1)_0px_9px_30px] ${className}`}>
             <img src={images.Post1Image} alt="title" className='w-full object-cover object-center h-auto md:h-52 lg:h-48 xl:h-60' />
             <div className='p-5'>
-                <h2 className='font-roboto font-bold text-xl text-dark-soft mdtext-2xl lg:text-[28px]'>
+                <h2 className='font-roboto font-bold text-xl text-dark-soft md:text-2xl lg:text-[28px]'>
                     Future of Work
                 </h2>
                 <p className='text-dark-light mt-3 text-sm md:text-lg'>
--- src/constants/images.js
+++ src/constants/images.js
@@ -2,12 +2,14 @@ import Logo from '../assests/Logo.svg'
 import HeroImage from '../assests/HeroImage.svg'
 import Post1Image from '../assests/posts/post1.jpg'
 import PostProfileImage from '../assests/posts/post-profile.svg'
+import CtaImage from '../assests/CtaImage.jpg'
 
 const images = {
     Logo,
     HeroImage,
     Post1Image,
     PostProfileImage,
+    CtaImage,
 };
 
 export default images;
