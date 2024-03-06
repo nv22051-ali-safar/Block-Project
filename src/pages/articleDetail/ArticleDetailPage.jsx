e52fe5c676b05f8846c679e8cd183324ec411399
nv22051-ali-safar <147384419+nv22051-ali-safar@users.noreply.github.com>
2024-03-06 18:55:53 +0000

    Comments section with replying

--- src/pages/articleDetail/ArticleDetailPage.jsx
+++ src/pages/articleDetail/ArticleDetailPage.jsx
@@ -4,6 +4,7 @@ import BreadCrumbs from '../../components/BreadCrumbs'
 import { images } from '../../constants'
 import SuggestedPosts from './container/SuggestedPosts'
 import { Link } from 'react-router-dom';
+import CommentsContainer from '../../components/comments/CommentsContainer'
 
 const breadCrumbsData = [
     { name: "Home", link: '/' },
@@ -72,6 +73,7 @@ const ArticleDetailPage = () => {
                             lectus proin.
                         </p>
                     </div>
+                    <CommentsContainer className="mt-10" logginedUserId="a" />
                 </article>
                 <SuggestedPosts header="Latest Article" posts={postsData} tags={tagsData} className="mt-8 lg:mt-0 lg:max-w-xs"/>
             </section>
