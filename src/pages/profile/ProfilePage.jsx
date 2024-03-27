2b13cd4051b9ad6fab80a32e2abdbab49983f1ce
nv22051-ali-safar <147384419+nv22051-ali-safar@users.noreply.github.com>
2024-03-27 12:47:43 +0000

    26 - Update user profile

--- src/pages/profile/ProfilePage.jsx
+++ src/pages/profile/ProfilePage.jsx
@@ -1,23 +1,43 @@
-import React, {useEffect} from 'react'
+import React, { useEffect } from 'react'
 import { useForm } from 'react-hook-form'
 import { useNavigate } from "react-router-dom"
 import MainLayout from '../../components/MainLayout'
 import { useDispatch, useSelector } from 'react-redux'
-import { useQuery } from '@tanstack/react-query'
-import {getUserProfile} from '../../services/index/users'
+import { useMutation, useQuery, useQueryClient } from '@tanstack/react-query'
+import { getUserProfile, updateProfile } from '../../services/index/users'
 import ProfilePicture from '../../components/ProfilePicture'
 
 const ProfilePage = () => {
     const navigate = useNavigate();
     const dispatch = useDispatch();
+    const queryClient = useQueryClient();
     const userState = useSelector(state => state.user)
 
-    const {data: profileData, isLoading: profileIsLoading, error: profileError} = useQuery({
+    const { data: profileData, isLoading: profileIsLoading, error: profileError } = useQuery({
         queryFn: () => {
-            return getUserProfile({token: userState.userInfo.token})
+            return getUserProfile({ token: userState.userInfo.token })
         },
         queryKey: ['profile']
-    })
+    });
+
+    const { mutate, isLoading } = useMutation({
+        mutationFn: ({ name, email, password }) => {
+            return updateProfile({
+                token: userState.userInfo.token,
+                userData: { name, email, password },
+            })
+        },
+        onSuccess: (data) => {
+            dispatch(userActions.setUserInfo(data));
+            localStorage.setItem('account', JSON.stringify(data))
+            queryClient.invalidateQueries(['profile'])
+            toast.succcess('Profile is updated')
+        },
+        onError: (error) => {
+            toast.error(error.message)
+            console.log(error);
+        },
+    });
 
     useEffect(() => {
         if (!userState.userInfo) {
@@ -39,11 +59,14 @@ const ProfilePage = () => {
     })
 
     const submitHandler = (data) => {
+        const { name, email, password } = data;
+        mutate({ name, email, password })
     };
 
     return <MainLayout>
         <section className='container mx-auto px-5 py-10'>
             <div className='w-full max-w-sm mx-auto'>
+                <p>{profileData?.name}</p>
                 <ProfilePicture avatar={profileData?.avatar} />
                 <form onSubmit={handleSubmit(submitHandler)}>
                     <div className='flex flex-col mb-6 w-full '>
@@ -87,18 +110,10 @@ const ProfilePage = () => {
                     </div>
 
                     <div className='flex flex-col mb-6 w-full '>
-                        <label htmlFor='password' className='text-[#5a7184] font-semibold block'>Password</label>
-                        <input type='password' id='password' {...register("password", {
-                            required: {
-                                value: true,
-                                message: "Password is required"
-                            },
-                            minLength: {
-                                value: 6,
-                                message: "Password length must be at least 6 characters"
-                            }
-                        })} placeholder='Enter password' className={`placeholder:text-[#959ead] text-dark-hard mt-3 rounded-lg px-5 py-4 font-semibold block outline-none border ${errors.password ? "border-red-500" : "border-[#c3cad9]"}`}
+                        <label htmlFor='password' className='text-[#5a7184] font-semibold block'>New Password {optional} </label>
+                        <input type='password' id='password' {...register("password")} placeholder='Enter new password' className={`placeholder:text-[#959ead] text-dark-hard mt-3 rounded-lg px-5 py-4 font-semibold block outline-none border ${errors.password ? "border-red-500" : "border-[#c3cad9]"}`}
                         />
+
                         {errors.password?.message && (
                             <p className='text-red-500 text-xs mt-1'>
                                 {errors.password?.message}
