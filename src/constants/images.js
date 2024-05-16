daad53dc8910c6c4b4bf9d145c73b1ec8622f8a6
nv22051-ali-safar <147384419+nv22051-ali-safar@users.noreply.github.com>
2024-05-16 17:04:08 +0000

    34 - show all posts in home page

--- src/constants/images.js
+++ src/constants/images.js
@@ -1,15 +1,19 @@
-import Logo from '../assests/Logo.svg'
-import HeroImage from '../assests/HeroImage.svg'
-import Post1Image from '../assests/posts/post1.jpg'
-import PostProfileImage from '../assests/posts/post-profile.svg'
-import CtaImage from '../assests/CtaImage.jpg'
+import Logo from "../assets/Logo.svg";
+import HeroImage from "../assets/HeroImage.svg";
+import Post1Image from "../assets/posts/post1.jpg";
+import PostProfileImage from "../assets/posts/post-profile.svg";
+import CtaImage from "../assets/CtaImage.jpg";
+const samplePostImage = "/images/sample.jpg";
+const userImage = "/images/user.png";
 
 const images = {
-    Logo,
-    HeroImage,
-    Post1Image,
-    PostProfileImage,
-    CtaImage,
+  Logo,
+  HeroImage,
+  Post1Image,
+  PostProfileImage,
+  CtaImage,
+  samplePostImage,
+  userImage,
 };
 
 export default images;
